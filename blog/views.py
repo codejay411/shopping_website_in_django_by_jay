9b505ac036ceb54fd74de5e140860ae1c762b9e6
codejay411 <jaypr202@gmail.com>
2020-07-26 10:59:49 +0530

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blogpost
+
+# Create your views here.
+def index(request):
+    # return HttpResponse("Index shop")
+    myposts=Blogpost.objects.all()
+    print(myposts)
+    return render(request, 'blog/index.html',{'myposts':myposts})
+
+
+def blogpost(request, id):
+    post = Blogpost.objects.filter(post_id = id)[0]
+    print(post)
+    return render(request, 'blog/blogpost.html',
+                  {'post':post})
